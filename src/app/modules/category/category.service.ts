7d722f77b4599d43fb252954e4a9f1dcb3b46121
Md: Mohon <mdmohon6145@gmail.com>
2023-09-13 00:43:39 -0400

    category done

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,86 +1,85 @@
-import { Category } from "@prisma/client";
-import httpStatus from "http-status";
-import ApiError from "../../../errors/ApiError";
-import prisma from "../../../shared/prisma";
+import { Category } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import prisma from '../../../shared/prisma';
 
 // create
-const createCategory = async(data:Category):Promise<Category>=>{
-    const result = await prisma.category.create({
-        data
-    })
-    return result
-}
+const createCategory = async (data: Category): Promise<Category> => {
+  const result = await prisma.category.create({
+    data,
+  });
+  return result;
+};
+
 // update
 const updateCategory = async (
-    id: string,
-    data: Partial<Category>
-  ): Promise<Category | null> => {
-    const result = await prisma.category.update({
-           where:{
-            id
-           },
-           include:{
-          books:true
-           },
-           data
-    })
-    return result
+  id: string,
+  data: Partial<Category>
+): Promise<Category | null> => {
+  const result = await prisma.category.update({
+    where: {
+      id,
+    },
+    include: {
+      books: true,
+    },
+    data,
+  });
+  return result;
+};
+// get all
+const getallcateGories = async (): Promise<Category[] | null> => {
+  const result = await prisma.category.findMany({
+    include: {
+      books: true,
+    },
+  });
+  if (!result) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'something went wrong');
   }
-  // get all
-  const getallcateGories = async (): Promise<Category[] | null> => {
-    const result = await prisma.category.findMany({
-        include:{
-            books:true
-        }
-    })
-    if (!result) {
-      throw new ApiError(httpStatus.BAD_REQUEST, 'something went wrong')
-    }
-    return result
-  }
-  
-  // get single user
-  const getsingleCategory = async (id: string): Promise<Category | null> => {
-    const result = await prisma.category.findUnique({
-        where:{
-            id
-        },
-        include:{
-            books:true
-        }
-    })
-    return result
-  }
+  return result;
+};
 
-  // delete
-  const deleteCategory = async (id: string): Promise<Category | null> => {
+// get single user
+const getsingleCategory = async (id: string): Promise<Category | null> => {
+  const result = await prisma.category.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      books: true,
+    },
+  });
+  return result;
+};
 
-   const result =  await prisma.$transaction(async (tx)=>{
-     await tx.book.deleteMany({
-      where:{
-        categoryId:id
-      }
-     })
-     const result = await tx.category.delete({
-      where:{
-          id
+// delete
+const deleteCategory = async (id: string): Promise<Category | null> => {
+  const result = await prisma.$transaction(async tx => {
+    await tx.book.deleteMany({
+      where: {
+        categoryId: id,
       },
-      include:{
-          books:true
-      }
-  })
-  return result
-    })
-  
-    return result
-  }
-  
+    });
+    const result = await tx.category.delete({
+      where: {
+        id,
+      },
+      include: {
+        books: true,
+      },
+    });
+    return result;
+  });
 
-const categoryServices  = {
-    createCategory,
-    updateCategory,
-    getallcateGories,
-    getsingleCategory,
-    deleteCategory
-}
-export default categoryServices
+  return result;
+};
+
+const categoryServices = {
+  createCategory,
+  updateCategory,
+  getallcateGories,
+  getsingleCategory,
+  deleteCategory,
+};
+export default categoryServices;
