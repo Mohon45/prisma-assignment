f773ea9b0d1225af5d79559fc266e7aa5fa4406b
Md: Mohon <mdmohon6145@gmail.com>
2023-09-13 00:45:24 -0400

    book done

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,226 +1,213 @@
-import { Book, Prisma } from "@prisma/client"
-import httpStatus from "http-status"
-import ApiError from "../../../errors/ApiError"
-import { paginationHelpers } from "../../../helpers/paginationHelper"
-import { IGenericResponse } from "../../../interfaces/common"
-import { IPaginationOptions } from "../../../interfaces/pagination"
-import prisma from "../../../shared/prisma"
-import { booksSearchableFields } from "./book.constant"
+import { Book, Prisma } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../shared/prisma';
+import { booksSearchableFields } from './book.constant';
 
-const createBooks = async(data:Book):Promise<Book>=>{
-    const result = await prisma.book.create({
-        data,
-        include:{
-            category:true,
-           
-        }
-    })
-    return result
-}
-const getallbooks = async (filters:any,options:IPaginationOptions): Promise<IGenericResponse<Book[] | null>> => {
-    const {search,minPrice,maxPrice,category} =  filters
-   
-    const {limit,skip,page} =  paginationHelpers.calculatePagination(options)
+const createBooks = async (data: Book): Promise<Book> => {
+  const result = await prisma.book.create({
+    data,
+    include: {
+      category: true,
+    },
+  });
+  return result;
+};
+const getallbooks = async (
+  filters: any,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Book[] | null>> => {
+  const { search, minPrice, maxPrice, category } = filters;
 
-    const andConditions =[]
+  const { limit, skip, page } = paginationHelpers.calculatePagination(options);
 
-    if(search){
-        andConditions.push({
-            OR:booksSearchableFields.map((field)=>({
-                [field]:{
-                    contains:search,
-                    mode:"insensitive"
-                }
-            }))
-        })
-    }
+  const andConditions = [];
 
-    if (minPrice !== undefined) {
-    
-      const minPrices = parseFloat(minPrice);
-  
-      if (!isNaN(minPrices)) {
-          andConditions.push({
-              price: {
-                  gte: minPrices,
-              },
-          });
-          console.log("MinPrice is used");
-      }
-  }
-  
-  if (maxPrice !== undefined) {
-   
-      const maxPrices = parseFloat(maxPrice);
-  
-      if (!isNaN(maxPrices)) {
-          andConditions.push({
-              price: {
-                  lte: maxPrices,
-              },
-          });
-          console.log("MaxPrice is used");
-      }
-  }
-  
-      
-if(category !==undefined){
+  if (search) {
     andConditions.push({
-        categoryId:{
-            equals:category,
-          
-        }
-    })
-}
-
- const whereConditions: Prisma.BookWhereInput =
-        andConditions.length > 0 ? { AND: andConditions } : {};
-    const count  =  await prisma.book.count({
-        where:whereConditions
-    })
-    const result = await prisma.book.findMany({
-        where:whereConditions,
-        include:{
-            category:true
+      OR: booksSearchableFields.map(field => ({
+        [field]: {
+          contains: search,
+          mode: 'insensitive',
         },
-        skip,
-        take:limit
-    })
-    if (!result) {
-      throw new ApiError(httpStatus.BAD_REQUEST, 'something went wrong')
-    }
-    return {
-        meta: {
-            total:count,
-            page,
-            limit
+      })),
+    });
+  }
+
+  if (minPrice !== undefined) {
+    const minPrices = parseFloat(minPrice);
+
+    if (!isNaN(minPrices)) {
+      andConditions.push({
+        price: {
+          gte: minPrices,
         },
-        data: result
-    };
+      });
+      console.log('MinPrice is used');
     }
-  
+  }
 
-    // get books by category id 
+  if (maxPrice !== undefined) {
+    const maxPrices = parseFloat(maxPrice);
 
-    const getBooksByCategoryId  = async(id:string,options:IPaginationOptions):Promise<IGenericResponse<Book[]|null>>=>{
-        const {limit,skip,page} =  paginationHelpers.calculatePagination(options)
-        const total =  await prisma.book.count({
-            where:{
-              
-                    categoryId:{
-                        equals:id
-                    }
-            
-            }
-        })
-        const result  =  await prisma.book.findMany({
-            where:{
-                categoryId:{
-                    equals:id
-                }
-            },
-            skip,
-            take:limit,
-            include:{
-                category:true
-            }
-        })
-
-     return {
-            meta: {
-                total,
-                page,
-                limit
-            },
-            data: result
-        };
+    if (!isNaN(maxPrices)) {
+      andConditions.push({
+        price: {
+          lte: maxPrices,
+        },
+      });
+      console.log('MaxPrice is used');
     }
+  }
 
+  if (category !== undefined) {
+    andConditions.push({
+      categoryId: {
+        equals: category,
+      },
+    });
+  }
 
-    // get single book 
-    const getsingleBook = async (id: string): Promise<Book | null> => {
-        const result = await prisma.book.findUnique({
-            where:{
-                id
-            },
-            include:{
-                category:true
-            }
-        })
-        return result
-      }
+  const whereConditions: Prisma.BookWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+  const count = await prisma.book.count({
+    where: whereConditions,
+  });
+  const result = await prisma.book.findMany({
+    where: whereConditions,
+    include: {
+      category: true,
+    },
+    skip,
+    take: limit,
+  });
+  if (!result) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'something went wrong');
+  }
+  return {
+    meta: {
+      total: count,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
 
+// get books by category id
 
+const getBooksByCategoryId = async (
+  id: string,
+  options: IPaginationOptions
+): Promise<IGenericResponse<Book[] | null>> => {
+  const { limit, skip, page } = paginationHelpers.calculatePagination(options);
+  const total = await prisma.book.count({
+    where: {
+      categoryId: {
+        equals: id,
+      },
+    },
+  });
+  const result = await prisma.book.findMany({
+    where: {
+      categoryId: {
+        equals: id,
+      },
+    },
+    skip,
+    take: limit,
+    include: {
+      category: true,
+    },
+  });
 
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
 
-    //   update single books 
-    const updateBook = async (
-        id: string,
-        data: Partial<Book>
-      ): Promise<Book | null> => {
-        const result = await prisma.book.update({
-               where:{
-                id
-               },
-               include:{
-              category:true
-               },
-               data
-        })
-        return result
-      }
+// get single book
+const getsingleBook = async (id: string): Promise<Book | null> => {
+  const result = await prisma.book.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      category: true,
+    },
+  });
+  return result;
+};
 
-        // delete
-        const deleteBook = async (id: string): Promise<Book | null> => {
-            let deletedBook: Book | null = null;
-          
-            await prisma.$transaction(async (prismaClient) => {
-        
-          
-   
-              const ordersWithBook = await prismaClient.order.findMany({
-                where: {
-                  orderedBooks: {
-                    some: {
-                      bookId: id,
-                    },
-                  },
-                },
-              });
-          
-      
-              await Promise.all(
-                ordersWithBook.map(async (order) => {
-                  await prismaClient.orderedBook.deleteMany({
-                    where: {
-                      orderId: order.id,
-                      bookId: id,
-                    },
-                  });
-                })
-              );
-              deletedBook = await prismaClient.book.delete({
-                where: {
-                  id,
-                },
-                include: {
-                  category: true,
-                },
-              });
-            });
-          
-     
-          
-            return deletedBook;
-          };
-          
-const booksServices ={
-    createBooks,
-    getallbooks,
-    getBooksByCategoryId,
-    getsingleBook,
-    updateBook,
-    deleteBook
+//   update single books
+const updateBook = async (
+  id: string,
+  data: Partial<Book>
+): Promise<Book | null> => {
+  const result = await prisma.book.update({
+    where: {
+      id,
+    },
+    include: {
+      category: true,
+    },
+    data,
+  });
+  return result;
+};
 
-}
-export default booksServices
+// delete
+const deleteBook = async (id: string): Promise<Book | null> => {
+  let deletedBook: Book | null = null;
+
+  await prisma.$transaction(async prismaClient => {
+    const ordersWithBook = await prismaClient.order.findMany({
+      where: {
+        orderedBooks: {
+          some: {
+            bookId: id,
+          },
+        },
+      },
+    });
+
+    await Promise.all(
+      ordersWithBook.map(async order => {
+        await prismaClient.orderedBook.deleteMany({
+          where: {
+            orderId: order.id,
+            bookId: id,
+          },
+        });
+      })
+    );
+    deletedBook = await prismaClient.book.delete({
+      where: {
+        id,
+      },
+      include: {
+        category: true,
+      },
+    });
+  });
+
+  return deletedBook;
+};
+
+const booksServices = {
+  createBooks,
+  getallbooks,
+  getBooksByCategoryId,
+  getsingleBook,
+  updateBook,
+  deleteBook,
+};
+export default booksServices;
