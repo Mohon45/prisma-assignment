ab1bb82751c775e17c60d6bbcd28ae0733972d64
Md: Mohon <mdmohon6145@gmail.com>
2023-09-13 00:44:28 -0400

    category bug fixed

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -1,68 +1,63 @@
-import catchAsync from "../../../shared/catchAsync";
+import catchAsync from '../../../shared/catchAsync';
 
-import { Request, Response } from "express";
-import categoryServices from "./category.service";
+import { Request, Response } from 'express';
+import categoryServices from './category.service';
 
-const createCategory =  catchAsync(async(req:Request,res:Response)=>{
-const result  = await categoryServices.createCategory(req.body)
-res.send({
-   success:true,
-   statusCode:200,
-   message:"Category created successfully",
-   data:result
-})
-})
+const createCategory = catchAsync(async (req: Request, res: Response) => {
+  const result = await categoryServices.createCategory(req.body);
+  res.send({
+    success: true,
+    statusCode: 200,
+    message: 'Category created successfully',
+    data: result,
+  });
+});
 
 const updateCategory = catchAsync(async (req: Request, res: Response) => {
-    const result = await categoryServices.updateCategory(req.params.id, req.body)
-    res.send({
-      success: true,
-      statusCode: 200,
-      message: 'category updated   successfully',
-      data: result,
-    })
-  })
+  const result = await categoryServices.updateCategory(req.params.id, req.body);
+  res.send({
+    success: true,
+    statusCode: 200,
+    message: 'category updated   successfully',
+    data: result,
+  });
+});
 
-  const getallcateGories = catchAsync(async (req: Request, res: Response) => {
+const getallcateGories = catchAsync(async (req: Request, res: Response) => {
+  const result = await categoryServices.getallcateGories();
+  res.send({
+    success: true,
+    statusCode: 200,
+    message: 'categories retrieved  successfully',
+    data: result,
+  });
+});
 
-    const result = await categoryServices.getallcateGories()
-    res.send({
-      success: true,
-      statusCode: 200,
-      message: 'categories retrieved  successfully',
-      data: result,
-    })
-  })
-  
-  // getsingle 
-  const getsingleCategory = catchAsync(async (req: Request, res: Response) => {
-    const result = await categoryServices.getsingleCategory(req.params.id)
-    res.send({
-      success: true,
-      statusCode: 200,
-      message: 'category retrieved  successfully',
-      data: result,
-    })
-  })
-  
+// getsingle
+const getsingleCategory = catchAsync(async (req: Request, res: Response) => {
+  const result = await categoryServices.getsingleCategory(req.params.id);
+  res.send({
+    success: true,
+    statusCode: 200,
+    message: 'category retrieved  successfully',
+    data: result,
+  });
+});
 
-
-  
-  const deleteCategory = catchAsync(async (req: Request, res: Response) => {
-    const result = await categoryServices.deleteCategory(req.params.id)
-    res.send({
-      success: true,
-      statusCode: 200,
-      message: 'category deleted   successfully',
-      data: result,
-    })
-  })
-const categoryController  =  {
-    createCategory,
-    updateCategory,
-    deleteCategory,
-getsingleCategory,
-    getallcateGories
-    
-}
-export default categoryController
+const deleteCategory = catchAsync(async (req: Request, res: Response) => {
+  const result = await categoryServices.deleteCategory(req.params.id);
+  res.send({
+    success: true,
+    statusCode: 200,
+    message: 'category deleted   successfully',
+    data: result,
+  });
+});
+const categoryController = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getsingleCategory,
+  getallcateGories,
+};
+export default categoryController;
